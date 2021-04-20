c6198e8fe4b9717b445e42db7fe8815ed7ca3333
KIM Hanseul <80247414+gkstmf012@users.noreply.github.com>
2021-04-20 17:10:40 +0900

    programmers

---
+++ practice#6.java
@@ -0,0 +1,30 @@
+https://programmers.co.kr/learn/courses/30/lessons/64061
+
+import java.util.Stack;
+
+class Solution {
+    public static int solution(int[][] board, int[] moves) {
+		
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i = 0; i < moves.length; i++) {
+			for(int j = 0; j<board.length; j++) {
+				if(board[j][moves[i]-1] != 0) {
+					if(!stack.empty() && stack.peek() == board[j][moves[i]-1]) {
+						answer++;
+						stack.pop();
+						board[j][moves[i]-1] = 0;
+						break;
+					}else {
+						stack.push(board[j][moves[i]-1]);
+						board[j][moves[i]-1] = 0;
+						break;
+					}
+				}
+			}
+		}
+		
+		return answer*2;
+	}
+}
